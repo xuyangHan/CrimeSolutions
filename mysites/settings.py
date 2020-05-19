ad4a9eb70afebde96b2cdc5b2de974d10ea760c6
Xuyang Han <hanxuyang0826@gmail.com>
2020-05-19 18:59:28 -0400

    updates

--- mysites/settings.py
+++ mysites/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_plotly_dash.middleware.BaseMiddleware',
 ]
 
 ROOT_URLCONF = 'mysites.urls'
