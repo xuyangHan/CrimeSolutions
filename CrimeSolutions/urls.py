edd03e27d80229028785de44e1b6ef909b5e9751
Xuyang Han <hanxuyang0826@gmail.com>
2020-05-19 19:05:00 -0400

    Update urls.py

--- CrimeSolutions/urls.py
+++ CrimeSolutions/urls.py
@@ -4,11 +4,11 @@ from .views import ListActivitiesView
 
 urlpatterns = [
     path("", views.launch, name="landing"),
-    path("map ", views.crime_map, name="idx_map"),
+    path("map", views.crime_map, name="idx_map"),
     path("index", views.index, name="index"),
     path("<int:activity_id>", views.activity, name="activity"),
     path("report", views.report, name="report"),
-    path("endorse <int:activity_id>", views.endorse, name="endorse"),
+    path("endorse<int:activity_id>", views.endorse, name="endorse"),
     path("signup", views.signup, name='signup'),
     path('user', views.user, name='user'),
     path('api/activities', ListActivitiesView.as_view(), name="songs-all")
